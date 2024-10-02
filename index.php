72c06f195172dc029d56ecbbbf3aacd028ed067d
natdm02 <natalidimarino2002@gmail.com>
2024-10-02 22:43:23 +0200

    [ADD]changes

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <!--bootstrap-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <!--css-->
-    <link rel="stylesheet" href="mine.css">
+    <link rel="stylesheet" href="./css/mine.css">
 </head>
 
 <body>
@@ -15,22 +15,24 @@
     <div class="container mt-5">
 
         <h2>
-            paragrafo e parola da censurare
+            INSERISCI:
         </h2>
 
         <form action="process.php" method="POST">
 
             <div class="form-group">
-                <textarea class="form-control" name="paragrafo" rows="5" placeholder="paragrafo" required></textarea>
+                <textarea class="form-control" name="paragrafo" rows="5" placeholder="UN PARAGRAFO:" required></textarea>
             </div>
 
             <div class="form-group">
-                <input type="text" class="form-control" name="parola" placeholder="parola" required>
+                <input type="text" class="form-control" name="parola" placeholder="UNA PAROLA DA CENSURARE: " required>
             </div>
 
-            <button type="submit" class="btn btn-primary">Invia</button>
+            <button type="submit" class="btn btn-primary">INVIA</button>
 
         </form>
+
     </div>
+
 </body>
 </html>
--- process.php
+++ process.php
@@ -1,23 +1,48 @@
 <?php
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
     $paragrafo = $_POST['paragrafo'];
 
     $parola = $_POST['parola'];
-
-
-    //1.paragrafo originale e lunghezza
-
-    echo "<h3>Paragrafo originale:</h3>";
-    echo "<p>$paragrafo</p>";
-    echo "<p>Lunghezza: " . strlen($paragrafo) . " caratteri</p>";
-
-    //2. censura
-    $paragrafo_censurato = str_ireplace($parola, '***', $paragrafo);
-
-    //2. stampo paragrafo  e lunghezza censurato
-    echo "<h3>Paragrafo censurato:</h3>";
-    echo "<p>$paragrafo_censurato</p>";
-    echo "<p>Lunghezza: " . strlen($paragrafo_censurato) . " caratteri</p>";
 }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RISULTATO</title>
+    <link rel="stylesheet" href="./css/mine.css">
+</head>
+
+<body>
+
+    <div class="container">
+
+        <h2>risultato:</h2>
+
+        <?php
+        //1.paragrafo originale e lunghezza
+
+        echo "<h3>Paragrafo originale:</h3>";
+        echo "<p>$paragrafo</p>";
+        echo "<p>Lunghezza: " . strlen($paragrafo) . " caratteri</p>";
+
+        //2. censura
+        $paragrafo_censurato = str_ireplace($parola, '***', $paragrafo);
+
+        //2. stampo paragrafo  e lunghezza censurato
+        echo "<h3>Paragrafo censurato:</h3>";
+        echo "<p>$paragrafo_censurato</p>";
+        echo "<p>Lunghezza: " . strlen($paragrafo_censurato) . " caratteri</p>";
+
+        ?>
+
+    </div>
+
+</body>
+
+</html>
 
